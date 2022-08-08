0f62e8bc08bc3127b8bd8fb4e25b8d70ba29db63
Heblg <110850858+Heblg@users.noreply.github.com>
2022-08-08 19:11:54 -0300

    Update caesar cipher.py

--- caesar cipher.py
+++ caesar cipher.py
@@ -1,6 +1,6 @@
 t = str(input("1 - Decode | 2 - Encode : "))
 cipher = list((str(input("enter the cipher: ")).lower()))
-cc = list((str(input("enter the cipher again: ")).lower()))
+cc = cipher.copy()
 alf = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"]
 print("\n")
 
